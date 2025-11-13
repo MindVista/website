9ce65d3d2380c2fe92b67d606ce9369175349f78
novruzoff <murad.novruzov1899@gmail.com>
2025-11-12 20:10:54 -0500

    fix(sponsor): hardcode logos for temporary fix

--- src/app/(app)/(pages)/sponsor/components/SponsorLogos.tsx
+++ src/app/(app)/(pages)/sponsor/components/SponsorLogos.tsx
@@ -5,16 +5,15 @@ interface SponsorLogosProps {
 }
 
 export function SponsorLogos({ sponsors }: SponsorLogosProps) {
+    const localLogos = ["/sponsor/caffettiera.png", "/sponsor/aym.webp", "/sponsor/wakewater.webp"];
+
     return (
         <>
-            {sponsors.map((sponsor) => {
-                const logo = sponsor.logo as Media | undefined;
-                return (
-                    <a key={sponsor.id} href={sponsor.url || undefined} target="_blank" rel="noreferrer" className="w-full transition-all duration-200 hover:scale-110">
-                        <img src={logo?.url || "/404.jpg"} alt={logo?.alt || "Sponsor logo"} className="mx-auto h-auto max-w-48 object-contain" loading="lazy" />
-                    </a>
-                );
-            })}
+            {sponsors.map((sponsor, i) => (
+                <a key={sponsor.id} href={sponsor.url || undefined} target="_blank" rel="noreferrer" className="flex w-full justify-center transition-all duration-200 hover:scale-110">
+                    <img src={localLogos[i] || "/404.jpg"} alt={`${sponsor.id} logo`} className="h-auto max-w-48 object-contain" loading="lazy" />
+                </a>
+            ))}
         </>
     );
 }
