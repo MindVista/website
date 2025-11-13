f2108e24e652112032935cbd75c15bc8abd2c060
novruzoff <murad.novruzov1899@gmail.com>
2025-11-12 20:32:19 -0500

    fix(sponsor): display wakewater logo

--- src/app/(app)/(pages)/sponsor/components/SponsorLogos.tsx
+++ src/app/(app)/(pages)/sponsor/components/SponsorLogos.tsx
@@ -1,19 +1,23 @@
-import { Media, Sponsor } from "@/payload-types";
+import { Sponsor } from "@/payload-types";
 
 interface SponsorLogosProps {
-    sponsors: Sponsor["sponsors"];
+    sponsors: Sponsor["sponsors"]; // still here but unused
 }
 
-export function SponsorLogos({ sponsors }: SponsorLogosProps) {
-    const localLogos = ["/sponsor/caffettiera.png", "/sponsor/aym.webp", "/sponsor/wakewater.webp"];
+export function SponsorLogos() {
+    const localLogos = [
+        { src: "/sponsor/caffettiera.png", url: "https://www.caffettiera.ca" },
+        { src: "/sponsor/aym.webp", url: "https://www.ashtangamontreal.com" },
+        { src: "/sponsor/wakewater.webp", url: "https://wakewater.ca" },
+    ];
 
     return (
-        <>
-            {sponsors.map((sponsor, i) => (
-                <a key={sponsor.id} href={sponsor.url || undefined} target="_blank" rel="noreferrer" className="flex w-full justify-center transition-all duration-200 hover:scale-110">
-                    <img src={localLogos[i] || "/404.jpg"} alt={`${sponsor.id} logo`} className="h-auto max-w-48 object-contain" loading="lazy" />
+        <div className="mx-auto flex items-center justify-center gap-16">
+            {localLogos.map((logo, i) => (
+                <a key={i} href={logo.url} target="_blank" rel="noreferrer" className="transition-all duration-200 hover:scale-110">
+                    <img src={logo.src} alt={`Sponsor ${i + 1} logo`} className="h-auto max-w-48 object-contain" loading="lazy" />
                 </a>
             ))}
-        </>
+        </div>
     );
 }
